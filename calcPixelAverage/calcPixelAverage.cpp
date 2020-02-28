cc59bceb3a3bcb4a8b81cb1b9af3a4b917904095
晨曦 <1179348863@qq.com>
2020-02-28 14:59:04 +0800

    threshold

--- calcPixelAverage/calcPixelAverage.cpp
+++ calcPixelAverage/calcPixelAverage.cpp
@@ -9,15 +9,22 @@ int main()
 {
 	CclassDemo enDemo;
 	int res = enDemo.maxVal(100, 99);
-	cv::Mat srcMat = imread("E:\\1.png");
+	cv::Mat srcMat = imread("E:\\2.png");
 	int height = srcMat.rows; //行数
 	int width = srcMat.cols; //每行元素的总元素数量
+	uchar threshold = 100;
 	for (int j = 0; j < height; j++) {
 		for (int i = 0; i < width; i++)
 		{
 			//处理每个像素
 			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] +
 			srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold) {
+				average = 255;
+			}
+			else {
+				average = 0;
+			}
 			srcMat.at<Vec3b>(j, i)[0] = average;
 			srcMat.at<Vec3b>(j, i)[1] = average;
 			srcMat.at<Vec3b>(j, i)[2] = average;
