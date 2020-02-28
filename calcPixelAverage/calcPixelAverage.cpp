55ca80a6f881fbdde41e487330d971f6bb27ab20
晨曦 <1179348863@qq.com>
2020-02-28 14:52:58 +0800

    imread0

--- calcPixelAverage/calcPixelAverage.cpp
+++ calcPixelAverage/calcPixelAverage.cpp
@@ -9,21 +9,8 @@ int main()
 {
 	CclassDemo enDemo;
 	int res = enDemo.maxVal(100, 99);
-	cv::Mat srcMat = imread("E:\\1.png");
-	int height = srcMat.rows; //行数
-	int width = srcMat.cols; //每行元素的总元素数量
-	for (int j = 0; j < height; j++) {
-		for (int i = 0; i < width; i++)
-		{
-			//处理每个像素
-			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] +
-			srcMat.at<Vec3b>(j, i)[2]) / 3;
-			srcMat.at<Vec3b>(j, i)[0] = average;
-			srcMat.at<Vec3b>(j, i)[1] = average;
-			srcMat.at<Vec3b>(j, i)[2] = average;
-			//结束像素处理
-		} //单行处理结束
-	}
+	cv::Mat srcMat = imread("E:\\2.png",0);
+
 	imshow("src", srcMat);
 	//等待用户按键
 	waitKey(0);
