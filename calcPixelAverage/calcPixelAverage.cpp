1854c158c719b70dd846630c25b067c7a629f506
晨曦 <1179348863@qq.com>
2020-02-28 15:08:53 +0800

    imread

--- calcPixelAverage/calcPixelAverage.cpp
+++ calcPixelAverage/calcPixelAverage.cpp
@@ -8,11 +8,10 @@ using namespace std;
 int main()
 {
 	CclassDemo enDemo;
-	int res = enDemo.maxVal(100, 99);
+//	int res = enDemo.maxVal(100, 99);
 	cv::Mat srcMat = imread("E:\\2.png",0);
 
 	imshow("src", srcMat);
-
 	//等待用户按键
 	waitKey(0);
 	return 0;
